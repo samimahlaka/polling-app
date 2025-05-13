1951486715be76c41173b8ea8e41f0c47959bd69
Mahlaka Sami <mahlaka@Mahlakas-MacBook-Pro.local>
2025-05-12 19:57:04 -0500

    Add Poll and Choice models with admin registration and migrations

---
+++ polls/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from .models import Poll, Choice
+
+admin.site.register(Poll)
+admin.site.register(Choice)
+
+
+
+# Register your models here.
---
+++ polls/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Poll(models.Model):
+    question = models.CharField(max_length=220)
+    created_by= models.ForeignKey(User, on_delete=models.CASCADE)
+    creted_at = models.DateTimeField( auto_now_add=True)
+    
+    def __str__(self):
+        return self.question
+    
+class Choice(models.Model):
+    poll = models.ForeignKey(Poll, on_delete=models.CASCADE, name='choices')
+    text = models.TextField(max_length=220)
+    vote = models.IntegerField(default=0)
+    
+    def __str__(self):
+        return self.text
+    
