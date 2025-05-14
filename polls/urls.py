05c677af6ecf5bb90fc29bdae4cf478ea0b3e5ab
Mahlaka Sami <mahlaka@Mahlakas-MacBook-Pro.local>
2025-05-14 13:53:48 -0500

    Add poll list API endpoint with PollSerializer

--- polls/urls.py
+++ polls/urls.py
@@ -6,4 +6,8 @@ urlpatterns = [
     
     path('<int:poll_id>/' , views.poll_detail, name = 'poll_detail'),
     path('api/<int:poll_id>/', views.get_poll),
+    path('vote/<int:choice_id>/', views.vote_submit),
+    path('results/<int:poll_id>/', views.poll_result),
+    path('poll_list/', views.poll_list),
 ]
+
--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from .models import Poll
+from .models import Poll, Choice
 from rest_framework.decorators import api_view
 from  rest_framework import status
 from .serializers import PollSerializer
@@ -14,3 +14,21 @@ def poll_detail(request, poll_id):
 def get_poll(request, poll_id):
     poll = get_object_or_404(Poll, id=poll_id)
     return Response(PollSerializer(poll).data)
+
+@api_view(['POST'])
+def vote_submit(request, choice_id):
+    choice = get_object_or_404(Choice, id = choice_id)
+    choice.vote += 1
+    choice.save()
+    return Response({'message' : 'Vote added' , 'votes':choice.vote})
+
+@api_view(['GET'])
+def poll_result(request, poll_id):
+    poll = get_object_or_404(Poll, id=poll_id)
+    return Response(PollSerializer(poll).data)
+
+@api_view(['GET'])
+def poll_list(request):
+    poll = Poll.objects.all()
+    return Response(PollSerializer(poll, many=True).data)
+        
