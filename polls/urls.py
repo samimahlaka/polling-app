b5075741a69cab42723aa0a15c92b22b657b8223
Mahlaka Sami <mahlaka@Mahlakas-MacBook-Pro.local>
2025-05-15 14:37:37 -0500

    Add poll list template with pagination and choices display

--- polls/urls.py
+++ polls/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path('api/<int:poll_id>/', views.get_poll),
     path('vote/<int:choice_id>/', views.vote_submit),
     path('results/<int:poll_id>/', views.poll_result),
-    path('poll_list/', views.poll_list),
-]
-
+    path('poll_list/', views.PollListView.as_view(), name = 'poll_list'),
+    path('page/',views.poll_list_template),
+    ]
--- polls/views.py
+++ polls/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Poll, Choice
 from rest_framework.decorators import api_view
-from  rest_framework import status
+from rest_framework import status
+from django.core.paginator import Paginator
 from .serializers import PollSerializer
 from rest_framework.response import Response
+from rest_framework.generics import ListAPIView
 
 def poll_detail(request, poll_id):
     poll= get_object_or_404(Poll, id  = poll_id)
@@ -27,8 +29,30 @@ def poll_result(request, poll_id):
     poll = get_object_or_404(Poll, id=poll_id)
     return Response(PollSerializer(poll).data)
 
-@api_view(['GET'])
-def poll_list(request):
+# @api_view(['GET'])
+# def poll_list(request):
+#     poll = Poll.objects.all()
+#     return Response(PollSerializer(poll, many=True).data)
+
+class PollListView(ListAPIView):
+    queryset = Poll.objects.all()
+    serializer_class = PollSerializer
+    
+    
+
+def poll_list_html(request):
+    page =  request.GET.get('page' , 1);
+    response = request.get(f'http://127.0.0.1:8000/polls/poll_list/?page={page}')
+    data = response.JSON()
+    return render(request,'poll_list.html',{'data' : data})
+
+
+def poll_list_template(request):
     poll = Poll.objects.all()
-    return Response(PollSerializer(poll, many=True).data)
+    page_number = request.GET.get('page')
+    paginator =Paginator(poll, 5)
+    page = paginator.get_page(page_number)
+    return render(request , 'polls/poll_list_template.html', {'page' : page})
+    
+    
         
